56e2558bb1f03164dba6f77967dce16405390dc3
wldlsmttn <64415937+wldlsmttn@users.noreply.github.com>
2025-01-24 15:20:26 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,13 +1,19 @@
-export default function Home() {
+export async function getShoes() {
+  const KV = await fetch(`https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/storage/kv/namespaces/${process.env.KV_SHOES_NAMESPACE}/values/shoes`, {
+    headers: {
+      'Authorization': `Bearer ${process.env.CLOUDFLARE_API_TOKEN}`,
+    }
+  });
+  return await KV.json();
+}
+
+export default async function Home() {
+  const shoes = await getShoes();
+
   return (
-    <div style={{
-      display: 'flex', 
-      justifyContent: 'center', 
-      alignItems: 'center', 
-      height: '100vh', 
-      fontSize: '3rem'
-    }}>
-      Coucou
-    </div>
+    <main className="p-4">
+      <h1>Comparateur de Chaussures</h1>
+      <pre>{JSON.stringify(shoes, null, 2)}</pre>
+    </main>
   );
 }
